069fdfb5240c336b166d45c36677d98d6b3b9354
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-15 10:37:40 +0530

    Group anagrams

---
+++ app/src/main/java/com/leetcode/arrays/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package com.leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/*Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]*/
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if (!result.contains(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(s);
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
+
