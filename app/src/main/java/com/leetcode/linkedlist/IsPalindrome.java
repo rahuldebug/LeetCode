1643eccb52b912ec886d8c9f1daf338c73e25b13
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-17 13:11:56 +0530

    Is Pallindrome

---
+++ app/src/main/java/com/leetcode/linkedlist/IsPalindrome.java
@@ -0,0 +1,44 @@
+package com.leetcode.linkedlist;
+
+public class IsPalindrome {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = reverse(slow.next);
+
+        fast = head;
+        while (slow != null){
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+
+    public ListNode reverse(ListNode node) {
+        ListNode cur = node;
+        ListNode prev = null;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
