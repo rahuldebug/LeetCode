be6b83dc8ad223c5939eb3ba72cc2bf42fa0b82f
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-17 12:43:36 +0530

    reverse singly linked list

--- app/src/main/java/com/leetcode/linkedlist/ReverseLinkedList.java
+++ app/src/main/java/com/leetcode/linkedlist/ReverseLinkedList.java
@@ -4,10 +4,10 @@ public class ReverseLinkedList {
     ListNode prev;
     public ListNode reverse(ListNode head){
         while(head!=null){
-            ListNode next=head.next;
-            head.next=prev;
-            prev=head;
-            head=next;
+            ListNode next=head.next;//reference to next node
+            head.next=prev; //reversing , pointing to new node
+            prev=head; //new node vakue assigned
+            head=next; //head pointed to next ref
 
         }
         return prev;
