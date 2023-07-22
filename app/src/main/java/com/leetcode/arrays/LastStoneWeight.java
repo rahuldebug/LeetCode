2d20b0f9f8b50df73241a9f54c8dd261582ed91c
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-22 14:44:28 +0530

    Last Stone weight

---
+++ app/src/main/java/com/leetcode/arrays/LastStoneWeight.java
@@ -0,0 +1,23 @@
+package com.leetcode.arrays;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    PriorityQueue<Integer>pq= new PriorityQueue<>(Collections.reverseOrder());
+    int lastStoneWeight(int[]nums){
+        for(int num:nums){
+            pq.add(num);
+        }
+        while (pq.size()>1){
+            int a= pq.poll();
+            int b= pq.poll();
+            if((a-b) !=0){
+                pq.add(a-b);
+            }
+        }
+        if(pq.size()==1) return pq.poll();
+        return 0;
+    }
+}
