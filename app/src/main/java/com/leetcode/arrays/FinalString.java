6dfa8debdf876144663a75179678d16589ae3e55
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-08-08 16:42:57 +0530

    Final String

---
+++ app/src/main/java/com/leetcode/arrays/FinalString.java
@@ -0,0 +1,22 @@
+package com.leetcode.arrays;
+/*Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have written. Typing other characters works as expected.
+
+You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+
+Return the final string that will be present on your laptop screen.*/
+public class FinalString {
+    StringBuilder sb = new StringBuilder();
+
+    public String finalString(String s) {
+        char[] S = s.toCharArray();
+
+        for (char ch : S) {
+            if (ch == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
