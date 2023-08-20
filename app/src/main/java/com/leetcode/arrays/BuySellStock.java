eee9c9be0ce1888a25ce0d0cf103307edd327786
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-08-20 19:49:08 +0530

    refactoring

--- app/src/main/java/com/leetcode/arrays/BuySellStock.java
+++ app/src/main/java/com/leetcode/arrays/BuySellStock.java
@@ -23,4 +23,15 @@ public class BuySellStock {
         }
         return profit;
     }
+//    public int maxProfit2(int[] prices) {
+//        int profit=0;
+//        int min=Integer.MAX_VALUE;
+//
+//        for(int price:prices){
+//            if(price<min) min=price;
+//            profit=Math.max(price-min,profit);
+//        }
+//        return profit;
+//
+//    }
 }
--- app/src/main/java/com/leetcode/arrays/RemoveDuplicateLinkedList.java
+++ app/src/main/java/com/leetcode/arrays/RemoveDuplicateLinkedList.java
@@ -36,4 +36,5 @@ public class RemoveDuplicateLinkedList {
             l1.remove(i);
         }
     }
+
 }
