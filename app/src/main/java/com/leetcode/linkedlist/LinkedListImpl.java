947dd00ad690bc2927d032ed5222cb184dd41520
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-09-11 13:31:08 +0530

    Linked List Implementation

---
+++ app/src/main/java/com/leetcode/linkedlist/LinkedListImpl.java
@@ -0,0 +1,51 @@
+package com.leetcode.linkedlist;
+
+public class LinkedListImpl {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        readList(node1);
+        readRecurse(node1);
+
+        ListNode head = null;
+        for (int i = 0; i < 10; i++) {
+            head = insertLast(head, i);
+        }
+        readRecurse(head);
+    }
+
+    static void readList(ListNode head) {
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    static void readRecurse(ListNode head) {
+        if (head == null) return;
+        System.out.println(head.val);
+        readRecurse(head.next);
+    }
+
+    static ListNode insertBegin(ListNode head, int x) {
+        ListNode temp = new ListNode(x);
+        temp.next = head;
+        return temp;
+    }
+
+    static ListNode insertLast(ListNode head, int x) {
+        ListNode temp = new ListNode(x);
+        if (head == null) return temp;
+        ListNode curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = temp;
+        return head;
+    }
+
+}
