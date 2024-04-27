3bf991d1cc505fec90f86627d160a62a5943f07f
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-04-27 15:21:31 +0530

    StackUsingArray

---
+++ app/src/main/java/com/leetcode/stackqueue/StackUsingArray.java
@@ -0,0 +1,41 @@
+package com.leetcode.stackqueue;
+
+public class StackUsingArray {
+    class MyStack {
+        int top;
+        int arr[];
+
+        MyStack() {
+            top = -1;
+            arr = new int[1000];
+        }
+
+        boolean isEmpty(){
+            return top==-1;
+        }
+
+        boolean isFull(){
+            return top==1000;
+        }
+
+        void push(int a) {
+            if(!isFull()){
+                top++;
+                arr[top]=a;
+            }
+
+
+
+        }
+
+        int pop() {
+            int result=-1;
+            if(!isEmpty()){
+                result= arr[top];
+                top--;
+                // return result ;
+            }
+            return result;
+        }
+    }
+}
