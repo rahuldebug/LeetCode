8666097add12a0681300cfb09845ffce8135df23
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-12 02:21:22 +0530

    Lower ceiling

---
+++ app/src/main/java/com/leetcode/binarysearch/LowerBound.java
@@ -0,0 +1,29 @@
+package com.leetcode.binarysearch;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        long[]arr={1,2,8,10,11,12,19};
+        int n = arr.length;
+        int x=5;
+        System.out.println( LowerBound.findFloor(arr,n,5));
+    }
+    public static int findFloor(long arr[], int n, long x)
+    {
+        int l=0;
+        int r=n-1;
+
+        int result=0;
+        while (l<=r){
+            int mid =l +  (r - l) /2 ;
+            if(arr[mid]<x){
+                result = mid;
+                l=mid+1;
+            } else {
+                r=mid-1;
+            }
+        }
+
+        return result;
+    }
+
+}
