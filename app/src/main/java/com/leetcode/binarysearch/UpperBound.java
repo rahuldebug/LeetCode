02c37255fad9930833457038c43a1d91888e20dd
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-12 02:40:22 +0530

    upper ceiling

---
+++ app/src/main/java/com/leetcode/binarysearch/UpperBound.java
@@ -0,0 +1,21 @@
+package com.leetcode.binarysearch;
+
+public class UpperBound {
+    public static int findCeil(long arr[], int n, long x) {
+        int result = -1;
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+        int mid=l + (r-l)/2;
+        if(arr[mid]>=x){
+            result=mid;
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+
+        }
+        return result;
+    }
+}
