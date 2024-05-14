b0bb9b787f52ee3947ca35afbbdb63471b75fcbb
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-14 13:41:02 +0530

    tree cutter

--- app/src/main/java/com/leetcode/binarysearch/TreeCutter.java
+++ app/src/main/java/com/leetcode/binarysearch/TreeCutter.java
@@ -1,4 +1,5 @@
 package com.leetcode.binarysearch;
+
 /*
 Tree Cutter Problem
 Binary Search
@@ -10,42 +11,35 @@ Since Geek is an environmentalist, he doesn't want to cut woods more than necess
 Find the maximum height H of the blade that still allows to cutoff at least M meters of wood.*/
 public class TreeCutter {
     public static void main(String[] args) {
-        int A[] = {4, 42, 40, 26, 46};
-        int M = 20;
+        int A[] = {47, 40, 21, 43, 33, 25, 7, 25, 40, 29, 16, 11, 3, 4, 42};
+        int M = 25;
         int N = A.length;
         TreeCutter treeCutter = new TreeCutter();
         System.out.println(treeCutter.maxHeight(N, A, M));
     }
 
-    public int maxHeight(int N, int[] A, int M) {
-        //You can code here
-        int low = 0;
-        int high = (int) 1e6;
-        while (low <= high) {
-            System.out.println("low : " + low + " high :"+high );
-            int mid = (int) (low + (high - low) / 2);
-            int val = cutTree(A, mid);
-            System.out.println("val is "+val);
-            if (val == M) {
-                return mid;
-            }
-            else if (val < M) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    public int getWoodAtCut(int[] A, int cut) {
+        int ans = 0;
+        for (int i = 0; i < A.length; i++) {
+            ans += A[i] > cut ? A[i] - cut : 0;
         }
-
-        return -1;
+        return ans;
     }
 
-    private int cutTree(int[] a, int mid) {
-        int sum = 0;
-        for (Integer e : a) {
-            if (mid < e) {
-                sum += e - mid;
+    public int maxHeight(int N, int[] A, int M) {
+        int high = (int) 1e9;
+        int low = 0;
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int totalWoodLength = getWoodAtCut(A, mid);
+            if (totalWoodLength >= M) {
+                result = mid; // Update the result with the current height
+                low = mid + 1; // Move towards higher heights
+            } else {
+                high = mid - 1; // Adjust the height lower
             }
         }
-        return sum;
+        return result;
     }
 }
