8d6c0e4ef28067ad06805c2224c147db6ad1a7f4
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-14 13:41:45 +0530

    tree cutter

---
+++ app/src/main/java/com/leetcode/stackqueue/ReverseStack.java
@@ -0,0 +1,32 @@
+package com.leetcode.stackqueue;
+
+import java.util.Stack;
+
+public class ReverseStack {
+//    public static void main(String[] args) {
+//        ReverseStack reverseStack= new ReverseStack();
+//        Stack<Integer> s= new Stack<>();
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//        s.push(4);
+//        s.push(5);
+//        for(Integer i:s){
+//            System.out.print(i);
+//        }
+//        reverseStack.reverseStacke(s,s.pop());
+//        for(Integer i:s){
+//            System.out.print(i);
+//        }
+//    }
+//
+//
+//    public void reverseStacke(Stack<Integer> s ,Integer e) {
+//        if(s.isEmpty()){
+//            s.push(e);
+//        }
+//        Integer temp=s.pop();
+//        reverseStacke(s,temp);
+//
+//    }
+}
