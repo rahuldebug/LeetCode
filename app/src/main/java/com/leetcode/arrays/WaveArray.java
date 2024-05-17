5146531fec41164bb8824cebe1fa077665cb60db
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-17 21:47:53 +0530

    left and right rotation

---
+++ app/src/main/java/com/leetcode/arrays/WaveArray.java
@@ -0,0 +1,36 @@
+package com.leetcode.arrays;
+
+import java.util.Arrays;
+
+/*Also called as wiggle sort , I have code to sort it in n log n , there is better way to sort it
+ * in N . Need to find that .
+Example:
+
+Given [1, 2, 3, 4]
+
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+
+NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
+So, in example case, you will return [2, 1, 4, 3]
+ *
+ * */
+public class WaveArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        int[] result = WaveArray.wiggleSort(arr);
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    static int[] wiggleSort(int arr[]) {
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
+        return arr;
+    }
+}
