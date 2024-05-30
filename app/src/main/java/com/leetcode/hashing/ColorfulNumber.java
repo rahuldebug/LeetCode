bd7bc6b41a9487e67600fc57ef687129f02904f0
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-30 15:04:17 +0530

    colorful , need to look at it once more

--- app/src/main/java/com/leetcode/hashing/ColorfulNumber.java
+++ app/src/main/java/com/leetcode/hashing/ColorfulNumber.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 public class ColorfulNumber {
     public static void main(String[] args) {
         ColorfulNumber colorfulNumber = new ColorfulNumber();
-        System.out.println(colorfulNumber.colorful(3757));
+       System.out.println(colorfulNumber.colorful(3757));
     }
 
     public int colorful(int A) {
@@ -18,7 +18,9 @@ public class ColorfulNumber {
             int product = 1;
             StringBuilder temp= new StringBuilder();
             for (int j = i; j < num.length(); j++) {
+                System.out.println("product at start :"+product);
                 temp.append(num.charAt(j));
+                System.out.println("product is :"+ product+" * "+ num.charAt(j) );
                 product = product * (num.charAt(j));
 //                System.out.println("the number is : "+ temp);
 //                System.out.println("the product is : "+product);
@@ -32,3 +34,33 @@ public class ColorfulNumber {
         return 1;
     }
 }
+
+/*
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int colorful(int A) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (A != 0) {
+            digits.add(A % 10);
+            A /= 10;
+        }
+        Collections.reverse(digits);
+        int n = digits.size();
+        Set<Integer> st = new HashSet<>();
+        for (int i = 0; i < n; ++i) {
+            int p = 1;
+            for (int j = i; j < n; ++j) {
+                p *= digits.get(j);
+                if (st.contains(p))
+                    return 0;
+                st.add(p);
+            }
+        }
+        return 1;
+    }
+}
+*/
