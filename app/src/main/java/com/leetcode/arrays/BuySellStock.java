00460c2f9477decdf9af31a1f12c5a2579fbef34
rahul_choudhary <rahulchoudhary.bit@gmail.com>
2024-08-02 13:17:07 +0530

    buy and sell the stock --1 leetcode -121

--- app/src/main/java/com/leetcode/arrays/BuySellStock.java
+++ app/src/main/java/com/leetcode/arrays/BuySellStock.java
@@ -1,15 +1,31 @@
 package com.leetcode.arrays;
 
-/*121. Best Time to Buy and Sell Stock
-Easy
-26.7K
-853
+/**
+Question
+121. Best Time to Buy and Sell Stock
+
 Companies
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+**/
+
+/** solution
+Brute force approach
+pick the current element on ith position and compare it with rest of the element to find the profit
+save the profit and compare it with global profit
+repeat the same for n-1 elements
+Time complexity == n^2
+keep evaluating the minimum and compare the current price and store it using math.max
+**/
+
+/**
+ * although an easy problem this form basis of Kadane's  algorithm
+ */
+
 public class BuySellStock {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
@@ -23,15 +39,5 @@ public class BuySellStock {
         }
         return profit;
     }
-//    public int maxProfit2(int[] prices) {
-//        int profit=0;
-//        int min=Integer.MAX_VALUE;
-//
-//        for(int price:prices){
-//            if(price<min) min=price;
-//            profit=Math.max(price-min,profit);
-//        }
-//        return profit;
-//
-//    }
+
 }
