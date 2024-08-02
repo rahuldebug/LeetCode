ca71c20b0328ac0d2694d19d2e37fa8fe08ed25d
rahul_choudhary <rahulchoudhary.bit@gmail.com>
2024-08-02 16:51:22 +0530

    buy and sell the stock --2 leetcode -122

--- app/src/main/java/com/leetcode/arrays/BuySellStock2.java
+++ app/src/main/java/com/leetcode/arrays/BuySellStock2.java
@@ -1,10 +1,8 @@
 package com.leetcode.arrays;
 
-/*122. Best Time to Buy and Sell Stock II
-Medium
-11.7K
-2.6K
-Companies
+/**
+122. Best Time to Buy and Sell Stock II(Medium)
+
 You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
 On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
@@ -16,7 +14,17 @@ Find and return the maximum profit you can achieve.
 Example 1:
 
 Input: prices = [7,1,5,3,6,4]
-Output: 7*/
+Output: 7
+
+ **/
+
+/**
+ Solution
+ Basically it is selling without a cool off period
+
+ if current price is greater than previous day ,sell and add to result
+
+ */
 public class BuySellStock2 {
     public int maxProfit(int[] prices) {
 
