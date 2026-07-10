a3990ee9588e62ddecc5a24d8d6b36300549c549
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2026-07-10 08:48:21 +0530

    factoria -- all three ways

---
+++ app/src/main/java/com/leetcode/recursion/Factorial.java
@@ -0,0 +1,32 @@
+package com.leetcode.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+       System.out.println(Factorial.tail(5));
+    }
+    
+    // 1. Loop (baseline)
+    public static long loop(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+     }
+    
+    // 2. Head Recursion (compute AFTER the call)
+    public static long head(int n) { 
+        if (n==0||n==1) return 1;
+        return n * head(n - 1);
+    }
+    
+    // 3. Tail Recursion (compute BEFORE, use accumulator)
+    public static long tail(int n) {
+        return tail(n, 1);
+    }
+
+    public static long tail(int n, long acc) {
+        if (n == 0 || n == 1) return acc;
+        return tail(n - 1, acc * n);
+    }
+}
