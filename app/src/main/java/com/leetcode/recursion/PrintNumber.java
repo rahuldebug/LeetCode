a98b279a39419bf6a0f3a9a21576b2cf0c64b9dd
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2026-09-12 18:03:18 +0530

    1st hour 12th sep

--- app/src/main/java/com/leetcode/recursion/PrintNumber.java
+++ app/src/main/java/com/leetcode/recursion/PrintNumber.java
@@ -3,6 +3,7 @@ package com.leetcode.recursion;
 public class PrintNumber {
 public static void main(String[] args) {
     funRev(10);
+    foo(3);
 }
 
 private static void funRev(int i) {
@@ -11,6 +12,14 @@ private static void funRev(int i) {
         System.out.println(i);
         funRev(i-1);
         System.out.println(i);
+    }
+}
+
+private static void foo(int n ){
+    if (n>0){
+        System.out.println(n);
+        foo(n-1);
+        foo(n-1);
     }
 }
 }
