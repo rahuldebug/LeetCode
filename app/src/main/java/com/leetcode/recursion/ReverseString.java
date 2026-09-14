c1c4162f986808cc934f2fe1aa6e79725291eb65
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2026-09-15 03:43:19 +0530

    added for pallindrome

--- app/src/main/java/com/leetcode/recursion/ReverseString.java
+++ app/src/main/java/com/leetcode/recursion/ReverseString.java
@@ -3,6 +3,8 @@ package com.leetcode.recursion;
 public class ReverseString {
     public static void main(String[] args) {
            String str="rahul";
+           String s="abcca";
+           System.out.println(ispallindrome(0,s.length()-1,s));
     System.out.println(reverse(str));
     }
 
@@ -17,6 +19,11 @@ public class ReverseString {
             if (length ==0) return sb;
             return reverseHelper(str, length-1, sb+str.charAt(length-1));
     }
- 
+
+    private static boolean ispallindrome(int l, int r , String s){
+        if (l>=r) return true;
+        if (s.charAt(l)!=s.charAt(r)) return false;
+        return ispallindrome(l+1,r-1,s);
+    }
 
 }
